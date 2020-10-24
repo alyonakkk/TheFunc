c51775ddb4e58b224a29f80dd01a25a40981bae7
alyonakkk <73276224+alyonakkk@users.noreply.github.com>
2020-10-24 23:14:51 +0500

    Добавьте файлы проекта.

---
+++ Laba2-1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Laba2_1
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStrip = new System.Windows.Forms.ToolStripButton();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStrip});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(809, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStrip.Image = ((System.Drawing.Image)(resources.GetObject("toolStrip.Image")));
+            this.toolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(171, 24);
+            this.toolStrip.Text = "Сформировать график";
+            this.toolStrip.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 30);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(809, 358);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(308, 409);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(189, 22);
+            this.textBox.TabIndex = 2;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 452);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form";
+            this.Text = "Func";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStrip;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Laba2-1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Laba2_1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private string surname = "Комарова";
+
+        [DllImport("Lib2-1.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private static extern double TheFunc(string surname, double x);
+
+        private static double OperA(string surname) // A
+        {
+            double x = TheFunc(surname, 1);
+            double y = TheFunc(surname, -1);
+            //double z = (x + y) / 2;
+            //double c = OperC(surname);
+            return (x + y) / 2;
+        }
+
+        private static double OperB(string surname) // B
+        {
+            double x = TheFunc(surname, 1);
+            double y = TheFunc(surname, -1);
+            return (x - y) / 2;
+        }
+
+        private static double OperC(string surname) // C
+        {
+            return TheFunc(surname, 0);
+        }
+
+        private void TheFunc1() //  y=a*x^2+b*x+c
+        {
+            cartesianChart1.AxisX.Clear(); 
+            cartesianChart1.AxisY.Clear();
+            ChartValues<ObservablePoint> points = new ChartValues<ObservablePoint>();
+            for (var x = -10; x <= 10; ++x)
+            {
+                points.Add(new ObservablePoint(x, OperA(surname) * Math.Pow(x, 2) + OperB(surname) * x + OperC(surname)));
+            }
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(new LineSeries
+            {
+                Values = points,
+            });
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            ChartValues<ObservablePoint> points = new ChartValues<ObservablePoint>();
+            textBox.Text = $"A = {OperA(surname)}\tB = {OperB(surname)}\tC = {OperC(surname)}";
+            for (int x = 0; x <= 10; x++)
+            {
+                points.Add(new ObservablePoint(x, TheFunc(surname, x)));
+            }
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(new LineSeries
+            {
+                Values = points,
+            });
+            TheFunc1();
+        }
+    }
+}
